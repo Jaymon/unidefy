06ad06a5900cc060ab884aa68473b1c0d1ecb81f
Jay Marcyes <jay@marcyes.com>
2013-06-26 18:53:11 -0700

    fixes the setup.py file, it might actually work this time

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ name = 'unidefy'
 version = ''
 with open('{}.py'.format(name), 'rU') as f:
     for node in (n for n in ast.parse(f.read()).body if isinstance(n, ast.Assign)):
-        name = node.targets[0]
-        if isinstance(name, ast.Name) and name.id.startswith('__version__'):
+        nname = node.targets[0]
+        if isinstance(nname, ast.Name) and nname.id.startswith('__version__'):
             version = node.value.s
             break
 
@@ -25,19 +25,8 @@ setup(
     author='Jay Marcyes',
     author_email='jay@marcyes.com',
     url='http://github.com/Jaymon/{}'.format(name),
-    py_modules=[
-        name,
-    ],
+    py_modules=[name],
     license="MIT",
-    zip_safe=True,
-    classifiers=[
-        'Development Status :: {}'.format(version),
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: MIT',
-        'Operating System :: OS Independent',
-        'Topic :: Debug',
-        ],
     test_suite = "test_{}".format(name),
 )
 
